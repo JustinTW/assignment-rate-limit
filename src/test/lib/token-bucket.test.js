9e88329ee631563619c24a5eca64b2410014af6c
Justin CT Liu <justintwd@gmail.com>
2018-10-29 19:27:38 +0800

    refactor(rate-limit): remove usless lines

--- src/test/lib/token-bucket.test.js
+++ src/test/lib/token-bucket.test.js
@@ -1,15 +1,15 @@
 /* eslint-env mocha */
 
-const express = require('express');
 const assert = require('assert');
-const request = require('supertest');
 const TokenBucket = require('../../lib/token-bucket.js');
 const MemoryStore = require('../../lib/token-bucket/memory-store');
 
 describe('token-bucket library', () => {
   it('should reduce one token', function() {
+    const settings = { limit: 3, interval: 100 };
     const tokenBucket = new TokenBucket({
-      store: new MemoryStore({ limit: 3 })
+      ...settings,
+      store: new MemoryStore({ limit: settings.limit })
     });
     tokenBucket.consume('127.0.0.1', (err, res) => {
       assert.equal(res, 1);
@@ -20,7 +20,7 @@ describe('token-bucket library', () => {
     const settings = { limit: -1, interval: 100 };
     const tokenBucket = new TokenBucket({
       ...settings,
-      store: new MemoryStore({ limit: -1 })
+      store: new MemoryStore({ limit: settings.limit })
     });
     tokenBucket.consume('127.0.0.1', (err, res) => {
       assert.equal(err);
